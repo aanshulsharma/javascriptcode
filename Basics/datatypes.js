cc29f325f3f3c3e3e8ec57818600868cab193e61
ANSHUL SHARMA <anshul.6061@gmail.com>
2023-09-24 06:15:02 +0000

    learned about datatypes in JS

---
+++ Basics/datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; // this means treat all the JavaScript code as newer version
+
+// alert(5+9) // we are using nodejs not browser that means alert method or function works on browser to show alerts
+
+let name = "Anshul"
+
+let age = 18
+
+let isLoggedIn = false
+
+
+
+
+//number => 2 to the power 23
+//bigint
+//string => ""
+//boolean => True/False
+// null is a standalone value 
+//undefined which means the value of that variable is not assigned
+//Symbols => whenever we talk about unique we use symbols
+
+// object
+
+console.log(typeof null); // output object
+console.log(typeof undefined); // output is undefined 
+
+//null is of object type
+//undefined is a type also
