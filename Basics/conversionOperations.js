66e5875c06a72a0a3c00df9ddf7f3620f0aa6fef
ANSHUL SHARMA <anshul.6061@gmail.com>
2023-10-06 05:22:44 +0000

    this is just a tutorial to understand how conversion works.

---
+++ Basics/conversionOperations.js
@@ -0,0 +1,10 @@
+let score = 33
+let rate = "33"
+
+console.log("Conversion And Operations");
+
+console.log(typeof score);
+console.log(typeof(score));
+
+console.log(typeof(rate));
+console.log(typeof rate);
